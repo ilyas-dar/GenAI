dc8887faa5ac30e2cadb6df3afdf81d67102c679
ilyas-dar <aleeilyas14@gmail.com>
2026-09-21 19:41:08 +0000

    added files of how to add any hugging face mode and also take an model from hugging face to local

--- chatmodels/huggingface.py
+++ chatmodels/huggingface.py
@@ -1,1 +1,14 @@
-from langchain import HuggingFaceHub, HuggingFaceEndpoint
+from dotenv import load_dotenv
+
+load_dotenv()  # Load environment variables from .env file
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+
+llm= HuggingFaceEndpoint(
+    repo_id="deepseek-ai/DeepSeek-R1"
+)
+
+
+model=ChatHuggingFace(llm=llm, temperature=0.7)
+
+response=model.invoke("tell me a joke not skeleton joke")
+print(response.content)
